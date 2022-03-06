9799edf65b2b6ee263155ea8a24bde3593403e48
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2022-03-06 22:37:01 +0800

    fix: 修复选框异常触发

--- src/utils/dragableElement.tsx
+++ src/utils/dragableElement.tsx
@@ -1,4 +1,7 @@
-import { CSSProperties, useCallback, useRef, useState } from "react";
+/**
+ * 这个HOC会阻止click的冒泡，react的点击事件会失效
+ */
+import { CSSProperties, useCallback, useEffect, useRef, useState } from "react";
 
 export const DragableElement: React.FC<{ style?: CSSProperties }> = (props) => {
   const [offsetX, setOffsetX] = useState(0);
@@ -6,6 +9,8 @@ export const DragableElement: React.FC<{ style?: CSSProperties }> = (props) => {
   const lastOffset = useRef({ x: 0, y: 0 });
   const isDraging = useRef(false);
   const temp = useRef({ x: 0, y: 0 });
+  const divEl = useRef<HTMLDivElement>(null);
+
   const onMouseDown = useCallback((e: React.MouseEvent) => {
     temp.current = { x: e.clientX, y: e.clientY };
     isDraging.current = true;
@@ -41,6 +46,15 @@ export const DragableElement: React.FC<{ style?: CSSProperties }> = (props) => {
     return newOffset;
   };
 
+  // 阻止事件被冒泡给react的事件合成，避免可拖拽组件上的click事件触发父组件的click，是个破坏性更改
+  // TODO: 这里对react侵入太大，使用这个HOC可能遇到坑（无法使用react绑定click事件），需要换成更好的写法而不是破坏react对点击事件的合成。
+  useEffect(() => {
+    const stopPropagation = (e: MouseEvent) => e.stopPropagation();
+    divEl.current?.addEventListener("click", stopPropagation);
+    return () =>
+      void divEl.current?.removeEventListener("click", stopPropagation);
+  }, []);
+
   return (
     <div
       style={{
@@ -49,6 +63,7 @@ export const DragableElement: React.FC<{ style?: CSSProperties }> = (props) => {
         top: offsetY,
         left: offsetX,
       }}
+      ref={divEl}
       onDragStart={onMouseDown}
       onDrag={onMouseMove}
       onDragEnd={onMouseUp}
