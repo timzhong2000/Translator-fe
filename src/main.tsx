eb3f5c4fadf8cd030eef2d3da2dbdaeb58fbfdf1
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2022-07-13 22:27:31 +0800

    feat: 引入normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
+import "normalize.css"
 
 ReactDOM.render(
   <React.StrictMode>
