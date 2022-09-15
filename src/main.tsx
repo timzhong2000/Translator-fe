82f69e323c585c01307f2b35648fdd299ffca234
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2022-09-16 01:59:13 +0800

    fix: 修复复制按钮样式

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
-import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
